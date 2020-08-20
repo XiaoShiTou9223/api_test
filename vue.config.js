0b435ffe7725c45091ed3e418e2fed12ed6d30cd
yangnan <yangnan@ienc.cn>
2020-08-20 17:15:51 +0800

    更新

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    runtimeCompiler: true,
+    filenameHashing: false,
+    devServer: {
+        open: false,
+        hotOnly: false,
+        host: '0.0.0.0',
+        port: 7100,
+        https: false
+    }
+}
